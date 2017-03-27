4486c7988a5fd9913fc64e3c5bde3d43c8fe22f6
Andrey <atsyganok83@gmail.com>
2017-03-27 14:40:11 +0300

    done

--- parser-cv_9-release_web/src/main/java/cloud/molddata/parser/cv/service/impl/MyUserDetailsService.java
+++ parser-cv_9-release_web/src/main/java/cloud/molddata/parser/cv/service/impl/MyUserDetailsService.java
@@ -28,15 +28,15 @@ public class MyUserDetailsService implements UserDetailsService {
 	@Transactional(readOnly=true)
 	@Override
 	public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
-
-		System.out.println("loadbyUSERNAME!!!!!!!!!!!!!!!!!!");
 		UserSecurity userSec = userService.findByUsername(username);
 		List<GrantedAuthority> authorities = buildUserAuthority(userSec.getUserSecRole());
 
 		return buildUserForAuthentication(userSec, authorities);
-		
+
 	}
 
+	// Converts com.parser.cv.model.User user to
+	// Converts com.parser.cv.model.User user to
 	// Converts com.parser.cv.model.User user to
 	// org.springframework.security.core.userdetails.User
 	private User buildUserForAuthentication(UserSecurity userSec, List<GrantedAuthority> authorities) {
@@ -46,8 +46,6 @@ public class MyUserDetailsService implements UserDetailsService {
 	private List<GrantedAuthority> buildUserAuthority(Set<UserSecRole> userRoles) {
 
 		Set<GrantedAuthority> setAuths = new HashSet<GrantedAuthority>();
-
-
 		// Build user's authorities
 		for (UserSecRole userRole : userRoles) {
 			setAuths.add(new SimpleGrantedAuthority(userRole.getRole().name()));
@@ -55,21 +53,14 @@ public class MyUserDetailsService implements UserDetailsService {
 		for (UserSecRole userRole : userRoles) {
 			String getRoleStr = null;
 			if(userRole.getRole().equals(UserRole.ROLE_USER)) {
-				//setAuths.add(new SimpleGrantedAuthority("ROLE_"+UserRole.ROLE_USER.name()));
 				getRoleStr = "ROLE_USER";
 			}else {
-				//setAuths.add(new SimpleGrantedAuthority(UserRole.ROLE_ADMIN.name()));
 				getRoleStr="ROLE_ADMIN";
 				System.out.println("role is ADMIN!!!");
 			}
 			setAuths.add(new SimpleGrantedAuthority(getRoleStr));
 		}
 
-		List<GrantedAuthority> Result = new ArrayList<GrantedAuthority>(setAuths);
-		for (GrantedAuthority so:Result){
-			System.out.println("roles for-each ="+so.getAuthority());
-		}
-
-		return Result;
+		return new ArrayList<GrantedAuthority>(setAuths);
 	}
 }
