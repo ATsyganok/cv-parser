e15e9e5e3f14fb35ffb9e8bbac0319aa61538e47
Andrey <atsyganok83@gmail.com>
2017-03-27 14:19:43 +0300

    done

--- parser-cv_9-release_web/src/main/java/cloud/molddata/parser/cv/parser/Paths.java
+++ parser-cv_9-release_web/src/main/java/cloud/molddata/parser/cv/parser/Paths.java
@@ -3,11 +3,12 @@ package cloud.molddata.parser.cv.parser;
 /**
  * Created by Андрей on 02.02.2017.
  */
-public final class Paths {
-    public static final String pfolderUploades="/opt/tomcat/temp/uploaded-files/";
-    //public static final String pfolderUploades="D:/P/uploaded-files/";
-    private static String pFolderDictionary= "/opt/tomcat/temp/dictionary/";
-    //public static String pFolderDictionary= "D:/P/dictionary/";
-    public static String pFolderPhones="/opt/tomcat/temp/location/";
-    //public static String pFolderPhones="D:/P/location/";
+public class Paths {
+    //public static final String pfolderUploades="/opt/tomcat/temp/uploaded-files/";
+    public static final String pfolderUploades="C:/P/uploaded-files/";
+    //public static String pFolderDictionary= "/opt/tomcat/temp/dictionary/";
+    public static String pFolderDictionary= "C:/P/dictionary/";
+    //public static String pFolderPhones="/opt/tomcat/temp/location/";
+    public static String pFolderPhones="C:/P/location/";
+    public Integer integer = Integer.MAX_VALUE;
 }
